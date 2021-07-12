3decf4d673a7a82891c0a6538b4a79353c50cd29
Abdullah-git-hub <74533464+Abdullah-git-hub@users.noreply.github.com>
2021-07-12 11:52:59 +0600

    Add files via upload

---
+++ txtTest.js
@@ -0,0 +1,21 @@
+function checkTxt(txt){
+    var slangs = ["aa", "bb", "cc", "dd", "ee"];
+    var txtArr = txt.split(" ");
+    var slangLen = slangs.length;
+    var txtLen = txtArr.length;
+    var finTxtArr = [];
+    for(i = 0; i < txtLen; i++){
+        var txtSlice1 = txtArr[i];
+        var lowCase = txtArr[i].toLowerCase();
+        var finTxtSlice = txtSlice1;
+        console.log(lowCase);
+
+        var isIncluded = slangs.includes(lowCase);
+        if(isIncluded){
+            finTxtSlice = "****";
+        }
+        finTxtArr.push(finTxtSlice);
+    }
+
+    return finTxtArr.join(" ");
+}
